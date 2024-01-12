b4b6943ad672c169c02d20be7a94fdf58c53cba0
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2024-01-12 13:23:25 -0700

    added get char report function
    - printed report

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ def main():
     text = get_book_text(path_to_file)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
-    print(f"{num_words} words found in the document")
+    print(f'''--- Begin report of {path_to_file} ---
+          {num_words} words found in the document 
+{get_chars_report(chars_dict)}
+          ''')
 
 
 def get_num_words(text):
@@ -26,6 +28,12 @@ def get_chars_dict(text):
             chars[lowered] = 1
     return chars
 
+def get_chars_report(dict):
+    char_list = []
+    for char in dict:
+        if char.isalpha():
+            char_list.append(f"The '{char}' was found {dict[char]} times ")
+    return "\n".join(sorted(char_list))
 
 
 main()
